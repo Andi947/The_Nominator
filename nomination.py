558eb8590dc715b3c5ef99bb5b9c9ae692fc3754
Andrea Naylor <andi.goldsworthy@gmail.com>
2017-11-04 11:57:54 +0000

    added comments to the User and Nomination classes

--- nomination.py
+++ nomination.py
@@ -1,14 +1,18 @@
+# Defines the variables of a Nomination object and the requirements to create/initialise a Nomination object.
 class Nomination(object):
 
+# variables that will make up a Nomination object (currently global variables - accessible by other classes calling this class)
     userID_nominating = 0
     nomineeID = 0
     reason = ""
 
+# Initialisation method for the Nomination object (when creating a Nomination object the variables userID, nomineeID and reason need to be passed)
     def __init__(self, userID, nomineeID, reason):
         self.userID_nominating = userID
         self.nomineeID = nomineeID
         self.reason = reason
 
+# Various Getter methods to get access to the value stored in the local variables in a Nomination object (currently they are global and don't require these methods)
     def get_userID(self):
         return self.userID_nominating
 
@@ -18,6 +22,7 @@ class Nomination(object):
     def get_reason(self):
         return self.reason
 
+# Various Setter methods to set the value of the local variables in the Nomination object (currently they are global and can be set without these methods)
     def set_userID(self, userID):
         self.userID_nominating = userID
 
--- user.py
+++ user.py
@@ -1,16 +1,20 @@
+# Defines the variables of a User object and the requirements to create/initialise a User object.
 class User(object):
 
+# variables that will make up a User object (currently global variables - accessible by other classes calling this class)
     userID = 0
     username = ""
     user_email = ""
     user_password = ""
 
+# Initialisation method for the User object (when creating a User object the variables userID, username, email and password need to be passed)
     def __init__(self, userID, username, email, password):
         self.userID = userID
         self.username = username
         self.user_email = email
         self.user_password = password
 
+# Various Getter methods to get access to the value of the local variables in User object (currently they are global and don't require these methods)
     def get_username(self):
         return self.username
 
@@ -23,6 +27,7 @@ class User(object):
     def get_userID(self):
         return self.userID
 
+# Various Setter methods to set the values of the local variables in the User object (currently they are global and can be set without these methods)
     def set_username(self, username):
         self.username = str(username)
 
@@ -31,6 +36,3 @@ class User(object):
 
     def set_user_password(self, password):
         self.user_password = str(password)
-
-    # def set_userID(self):
-    #     self.userID =
